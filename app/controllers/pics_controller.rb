edfd5d77f606f37c3ef80258bdcf5e781dd171be
ThomasDebek <tom.debek@gmail.com>
2022-02-21 21:03:31 +0100

    simple_form: rails generate simple_form:install --bootstrap

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,34 @@
 class PicsController < ApplicationController
+
 	def index
 		@pics = Pic.all 
+	end
+
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.new(pic_params)
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:title, :description)
 	end
 
 end
