11abd13c9f74eb5650eff5ce4a829efb12302692
ThomasDebek <tom.debek@gmail.com>
2022-06-27 21:07:46 +0200

    seed: generate some data

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "jbuilder"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem 'jquery-rails'
+gem 'faker'
 
 #add gems
 gem 'haml-rails', '~> 2.0', '>= 2.0.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Delete User and Pics"
+User.delete_all
+Pic.delete_all
+
+puts "Generating User and Pics"
+User.create(email: 'dave@gmail.com', password: 'secret')
+User.create(email: 'john@gmail.com', password: 'secret')
+User.create(email: 'nikodem@gmail.com', password: 'secret')
+User.create(email: 'jurek@gmail.com', password: 'secret')
+User.create(email: 'maciek@gmail.com', password: 'secret')
+
+5.times do
+  p = Pic.create(
+    title: Faker::Commerce.unique.product_name,
+    description: Faker::Hipster.sentences.sample,
+    user_id: rand(1..5)
+  )
+
+    puts "generating - piks - #{p.title}"
+    downloaded_image = URI.open("https://source.unsplash.com/700x400/?#{p.title.split.last}")
+    p.image.attach(io: downloaded_image, filename: "mi_#{p.id}.png")
+
+end
